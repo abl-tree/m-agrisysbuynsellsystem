eaa75c468726271049ff446efbc306ea5e8008a1
Ximdrake Asidor <asidorx18@gmail.com>
2025-12-10 18:21:47 +0800

    update

---
+++ app/Ca.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ca extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'cash_advance';
+    protected $fillable = array(
+		'customer_id',
+		'reason',
+		'amount',
+		'balance',
+    );
+    public $timestamps = true;
+
+    public function customer() {
+        return $this->hasOne('App\Customer', 'id', 'customer_id');
+    }
+  
+}
